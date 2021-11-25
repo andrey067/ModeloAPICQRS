095a0202c7f19ba61a1f5d88004ec1ad0030e4d0
Audrey  Ernesto de Lima <3970@ucdb.br>
2021-11-25 18:48:15 -0400

    Ajustando repository

---
+++ src/Api.Infrastructure/Context/MongoContext.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Infrastructure.Context
+{
+    public class MongoContext : IMongoContext
+    {
+        private IMongoDatabase Database { get; set; }
+        public IClientSessionHandle Session { get; set; }
+        public MongoClient MongoClient { get; set; }
+        private readonly List<Func<Task>> _commands;
+        private readonly IConfiguration _configuration;
+
+        public MongoContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            // Every command will be stored and it'll be processed at SaveChanges
+            _commands = new List<Func<Task>>();
+        }
+
+        public void AddCommand(Func<Task> func)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Api.Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Api.Domain.Entities;
+using Api.Domain.Interfaces;
+using Api.Repositories;
+
+namespace Api.Infrastructure.Context
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(IMongoContext context) : base(context)
+        {
+        }
+    }
+}
